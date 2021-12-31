86db6dc8a18f3995fb6cd1d625506112858ae743
Heemank Verma <heemankv@gmail.com>
2021-12-31 11:36:02 +0530

    re works

--- components/IntroComponent/index.js
+++ components/IntroComponent/index.js
@@ -3,6 +3,8 @@ import styles from "./styles.module.css";
 import img from "../../public/assets/images/intro.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faWhatsapp, faInstagram } from "@fortawesome/free-brands-svg-icons";
+
+import Zomato from "../../public/svg/zomato.svg";
 import Link from "next/link";
 import { Banner1, Banner2, Banner3, Banner4 } from "../../data";
 import Carousel from "react-multi-carousel";
@@ -33,79 +35,9 @@ function IntroComponent() {
 
   return (
     <>
-      {/* <div>
-      <AutoplaySlider
-        style={{ styles }}
-        play={true}
-        cancelOnInteraction={false} // should stop playing on user interaction
-        interval={1000}
-        buttons={false}
-        mobileTouch={true}
-
-      >
-        <div className="">
-          <Image src={Banner2} alt="sd" />
-        </div>
-
-        <div className="">
-          <Image src={Banner3} alt="sd" />
-        </div>
-
-        <div className="">
-          <Image src={Banner3} alt="sd" />
-        </div>
-
-      </AutoplaySlider>
-    </div> */}
-      {/* <Carousel
-        swipeable={true}
-        draggable={true}
-        showDots={false}
-        responsive={responsive}
-        ssr={true} // means to render carousel on server-side.
-        infinite={true}
-        autoPlay={true}
-        autoPlaySpeed={1000}
-        keyBoardControl={true}
-        customTransition="all .5"
-        transitionDuration={5000}
-        containerClass="carousel-container"
-        removeArrowOnDeviceType={["tablet", "mobile"]}
-        itemClass="carousel-item-padding-40-px"
-      >
-        <div>
-          <Image src={Banner1} alt="image"  />
-        </div>
-        <div>
-          <Image src={Banner2} alt="image" />
-        </div>
-        <div>
-          <Image src={Banner3} alt="image" />
-        </div>
-        <div>
-          <Image src={Banner1} alt="image"  />
-        </div>
-        <div>
-          <Image src={Banner2} alt="image" />
-        </div>
-        <div>
-          <Image src={Banner3} alt="image" />
-        </div>
-        <div>
-          <Image src={Banner1} alt="image"  />
-        </div>
-        <div>
-          <Image src={Banner2} alt="image" />
-        </div>
-        <div>
-          <Image src={Banner3} alt="image" />
-        </div>
-        
-      </Carousel> */}
-
-      <CarouselCover/>
-
+    <CarouselCover/>
     <div className={styles.iconsColumn}>
+        
         <Link href={"https://www.facebook.com"}>
           <a target="_blank">
             <FontAwesomeIcon
@@ -130,6 +62,48 @@ function IntroComponent() {
             <FontAwesomeIcon
               icon={faWhatsapp}
               size="3x"
+              style={{ color: "#25D366" }} />
+          </a>
+        </Link>
+      </div>
+
+      <div className={styles.iconsColumn2}>
+
+      <Link href={"https://www.zomato.com"}>
+          <a target="_blank">
+            <Image
+              src={Zomato}
+              size="3x"
+              style={{ color: "#006AFF" }} />
+          </a>
+        </Link>
+        <br />
+        
+
+        <Link href={"https://www.facebook.com"}>
+          <a target="_blank">
+            <FontAwesomeIcon
+              icon={faFacebook}
+              size="2x"
+              style={{ color: "#006AFF" }} />
+          </a>
+        </Link>
+        <br />
+
+        <Link href={"https://www.facebook.com"}>
+          <a target="_blank">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              size="2x"
+              style={{ color: "rgb(189, 105, 189)" }} />
+          </a>
+        </Link>
+        <br />
+        <Link href={"https://www.whatsapp.com"}>
+          <a target="_blank" className={styles.nav_item}>
+            <FontAwesomeIcon
+              icon={faWhatsapp}
+              size="2x"
               style={{ color: "#25D366" }} />
           </a>
         </Link>

d58c8026f1c3fb9c24330b56283b3215824e7ab7
Heemank Verma <heemankv@gmail.com>
2022-01-03 18:39:57 +0530

    final

--- components/TeamCard/index.js
+++ components/TeamCard/index.js
@@ -5,7 +5,9 @@ function TeamCard(props) {
   return (
     <div className={styles.wrapper}>
       <Image alt={"Gayatri eats food roziroti  dwarka photo check out restaraunt now  www.gayatrieats.com "} src={props.data.image}   className="imageDiv"  height="250" width="250"/>
-      <h2>{props.data.name}</h2>
+      <h2 style={{margin:0}}>{props.data.name}</h2>
+      <h4 style={{margin:0}}>Owner and Manager</h4>
+
     </div>
   );
 }

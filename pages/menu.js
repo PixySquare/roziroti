453d65480966d952875077aff346e61dcf5f90c2
Garvit Batra <gbatra145@gmail.com>
2022-02-23 20:32:15 +0530

    meta tags added to pages

--- pages/menu.js
+++ pages/menu.js
@@ -6,10 +6,23 @@ import MenuItem from "../components/MenuItem";
 import ContactUs from "../components/ContactUs";
 import Footer from "../components/Footer";
 import { AllMenu , Divider, MainCourse } from "../data";
-
+import Head from "next/head"
 function menu() {
   return (
-    <><div>
+    <>
+    <Head>
+        <title>Our Menu</title>
+          <meta name="og:description" content="RoziRoti is gayatri's new venture into the food world, providing people with low cost high quality food items in dwarka, Visit now to try out the best food in Dwarka "></meta>
+          <meta property="og:title" content="GayatriEats presents RoziRoti, an everyday food store."></meta>
+          <meta property="description" content="A love for food and a very special attachment to the coziness of home are the two central components of Rozi Roti. We're a bunch of people who are extremely passionate about food. We love to eat and feed just as well. On a serious note, at Rozi Roti, we believe that food shouldn't only be affordable and convenient but also fresh, fun and delicious. And what could be better than home-cooked food served at your disposal. Moreover we ourselves are food lovers & great foodies, hence we believe that cooking and catering is an art that is mastered through experience along with great passion for food. Rozi Roti brings to you sumptuous yet nutritious meals prepared by very talented chefs from the freshest seasonal ingredients and delivered to your workplace with unswerving promptness. And the best bit is affordable prices, since our food is your Craving."></meta>
+          {/* <meta property="og:url" content="https://gayatrieats.com/"></meta> */}
+          <meta property="og:type" content="website"></meta>
+          <meta name="robots" content="index, follow"></meta>
+          <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
+          {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+    <div>
+
       <Header />
       <div className={styles.cover}>
         <div className={styles.heading}>Our Menu</div>
